5082e299df1c79c4a8d48d469984a30bbdca05ed
1ncounter <yj7126@yeah.net>
2020-04-06 21:47:28 +0800

    first commit

---
+++ generator/template/mock/index.js
@@ -0,0 +1,7 @@
+/**
+ * mock默认主入口
+ * 本插件基于mockjs，支持对象与函数的template
+ * 文档：https://github.com/nuysoft/Mock/wiki/Syntax-Specification
+ */
+
+module.exports = {};
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+module.exports = (api, options) => {
+  const mockOptions = (options.pluginOptions && options.pluginOptions.mock) || {};
+  const entry = mockOptions.entry || 'mock/index.js';
+  mockOptions.entry = api.resolve(entry);
+
+  api.configureDevServer((app, server) => {
+    const prefix = mockOptions.prefix || '/mock';
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use(prefix, require('./lib/mock')(api, mockOptions));
+  });
+};
+
+module.exports.defaultModes = {
+  mock: 'development',
+};
